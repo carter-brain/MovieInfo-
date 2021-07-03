c0a3482546bb0c0e9647acd23e779096dd13c654
Leo_nickson <leonickson1@gmail.com>
2021-07-03 23:04:26 +0530

    code modified

--- App.js
+++ App.js
@@ -18,48 +18,18 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     const query = req.body.movie;
     const url = "https://www.omdbapi.com/?t=" + query + "&apikey=" + process.env.API_KEY + "&plot=full";
+
     axios.get(url).then(response => {
-        try{
         if (response.data.Response === "True") {
             const info = response.data;
-            var IMDB = "";
-            var Meta = "";
-            var Rotten = "";
-            for (let i = 0; i < info.Ratings.length; i++) {
-                if (info.Ratings[i].Source === "Internet Movie Database") {
-                    IMDB = info.Ratings[i].Value;
-                    break;
-                } else {
-                    IMDB = "N/A";
-                }
-            }
-            for (let i = 0; i < info.Ratings.length; i++) {
-                if (info.Ratings[i].Source === "Metacritic") {
-                    Meta = info.Ratings[i].Value;
-                    break;
-                } else {
-                    Meta = "N/A";
-                }
-            }
-            for (let i = 0; i < info.Ratings.length; i++) {
-                if (info.Ratings[i].Source === "Rotten Tomatoes") {
-                    Rotten = info.Ratings[i].Value;
-                    break;
-                } else {
-                    Rotten = "N/A";
-                }
-            }
-
-            res.render("result", { info: info, IMDB: IMDB, Meta: Meta, Rotten: Rotten });
+            res.render("result", { info: info, IMDB: typeof response.data.Ratings[0] === 'undefined'? "N/A" :response.data.Ratings[0].Value,
+             Meta: typeof response.data.Ratings[1] === 'undefined'? "N/A" :response.data.Ratings[2].Value,
+              Rotten:  typeof response.data.Ratings[2] === 'undefined'? "N/A" :response.data.Ratings[1].Value });
         }
         else {
             res.render("failure");
-        }}
-        catch{
-            res.render("failure");
         }
     });
-
 });
 app.post("/search", (req, res) => {
     res.redirect("/");
@@ -67,62 +37,3 @@ app.post("/search", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is up and running");
 });
-// //jshint esversion:6
-// require("dotenv").config();
-
-// const express = require("express");
-// const ejs = require("ejs");
-// const bodyParser = require("body-parser");
-// const request = require("request");
-
-// const app = express();
-// app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.set('view engine', 'ejs');
-
-
-// app.get("/", (req, res) => {
-//     res.render("index");
-// });
-// app.post("/", (req, res) => {
-//     const query = req.body.movie;
-//     const url = "https://www.omdbapi.com/?t=" + query + "&apikey=" + process.env.API_KEY + "&plot=full";
-//       request(url, function(error, response, body){
-//           const info = JSON.parse(body);
-//           var IMDB = "";
-//           var Meta = "";
-//           var Rotten = "";
-//           for(let i = 0; i < info.Ratings.length; i++){
-//             if(info.Ratings[i].Source ===  "Internet Movie Database"){
-//                 IMDB = info.Ratings[i].Value;
-//                 break;
-//             }else{
-//                 IMDB = "N/A";
-//             }
-//         }
-//         for(let i = 0; i < info.Ratings.length; i++){
-//             if(info.Ratings[i].Source ===  "Metacritic"){
-//                Meta = info.Ratings[i].Value;
-//                break;
-//             }else{
-//                 Meta = "N/A";
-//             }
-//         }
-//         for(let i = 0; i < info.Ratings.length; i++){
-//             if(info.Ratings[i].Source === "Rotten Tomatoes"){
-//                 Rotten = info.Ratings[i].Value;
-//                 break;
-//             }else{
-//                 Rotten = "N/A";
-//             }
-//         }
-
-//         res.render("result", {info: info, IMDB: IMDB, Meta: Meta, Rotten: Rotten});
-//     }); 
-
-// });
-
-
-// app.listen(process.env.PORT || 3000, ()=>{
-//     console.log("Server is up and running");
-// });
